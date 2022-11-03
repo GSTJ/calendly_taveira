4fa9070bfa61c0867887ee8f34deee88efe48b4d
Gabriel Taveira <gabrielstaveira@gmail.com>
2022-11-03 19:56:55 -0300

    fix: remove unnecessary appname prefixes from klasses and views

--- src/klasses/index.ts
+++ src/klasses/index.ts
@@ -1,9 +1,8 @@
-import { appName } from '../constants';
 import kapp from '../server/kapp';
 import { event } from './event';
 
-export const Klasses = {
-  Event: `${appName}-event`,
-};
+export enum Klasses {
+  Event = 'Event-Klass',
+}
 
 kapp.useKlass(Klasses.Event, event);
--- src/views/index.ts
+++ src/views/index.ts
@@ -1,11 +1,24 @@
-import { appName } from '../constants';
+import fs from 'fs';
+import path from 'path';
+
+import { Klasses } from '../klasses';
 import kapp from '../server/kapp';
 
-kapp.useView(`${appName}-event-kview`, './src/views/ExpandedTimeline.jsx', {
-  resource: 'kobject',
-  context: 'expanded-timeline',
-  displayName: 'Calendly Event',
-  icon: 'calendar',
-  state: 'open',
-  klass: 'calendly-sdk-event',
-});
+enum KViews {
+  EventKView = 'Event-KView',
+}
+
+kapp.useView(
+  KViews.EventKView,
+  fs.readFileSync(path.resolve(__dirname, './ExpandedTimeline.jsx'), {
+    encoding: 'utf-8',
+  }),
+  {
+    resource: 'kobject',
+    context: 'expanded-timeline',
+    displayName: 'Calendly Event',
+    icon: 'calendar',
+    state: 'open',
+    klass: Klasses.Event,
+  },
+);
